ead5c8f80e96fe3d9e8058da2dc917c304e6eac4
syed.sohail <syed.sohail@NuLaptop404.nuware.com>
2022-12-27 15:10:59 +0530

    List and Navigation

--- Model/Landmark.swift
+++ Model/Landmark.swift
@@ -4,38 +4,6 @@
 //
 //  Created by syed.sohail on 26/12/22.
 
-//
-//import Foundation
-//import SwiftUI
-//import CoreLocation
-//
-//struct Landmark: Hashable, Codable{
-//    var id: Int
-//    var name: String
-//    var state: String
-//    var description: String
-//    var park: String
-//
-//    private var imageName: String
-//    var image: Image {
-//        Image(imageName)
-//    }
-//
-//    private var coodinates: Coordinates
-//
-//    var locationCoordinates: CLLocationCoordinate2D{      //
-//        CLLocationCoordinate2D(
-//            latitude: coodinates.latitude,
-//            longitude: coodinates.longitute
-//        )
-//    }
-//
-//    struct Coordinates: Hashable, Codable {
-//        var longitute: Double
-//        var latitude: Double
-//    }
-//}
-
 import Foundation
 import SwiftUI
 import CoreLocation
